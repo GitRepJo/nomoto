83bf05159cd0e862b330c7fe35cb1cc516c67c75
mahler <mahler@dst-org.de>
2022-10-24 14:17:09 +0200

    copyleft

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-/**
-	Copyright (c) 2022 Jonas Mahler
+// Copyright (c) 2022 Jonas Mahler
 	
+// This file is part of nomoto.
+
+// nomoto is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 3 of the License, or (at your option) any later version.
+
+// nomoto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along 
+// with nomoto. If not, see <https://www.gnu.org/licenses/>. 
+
+/**	
     @name main.cpp
 	@brief An example of how to use nomoto_model.hpp and run Nomotos model
 	@author Jonas Mahler
